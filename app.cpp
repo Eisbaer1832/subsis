e18ce6e095d66ce760c49f82ae185c01ef2ca2d4
eisaber1832 <eisaber1832@gmail.com>
2022-08-16 19:52:47 +0200

    Program can now regocnize if train will come too late

--- app.cpp
+++ app.cpp
@@ -1,3 +1,9 @@
+// Some helpful wesbsites
+// https://de.statista.com/themen/123/deutsche-bahn/#topicHeader__wrapper
+// https://de.statista.com/statistik/daten/studie/254053/umfrage/zugunfaelle-innerhalb-der-eu-nach-unfallkategorie/
+
+
+
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -13,15 +19,16 @@ using namespace std;
 string train1, train2, train3;
 
 // intagers for example train 1
-int train1_left, train1_aproximet, train1_length, train1_current, train1_time, train1_speed;
+int train1_left, train1_aproximet, train1_length, train1_current, train1_time, train1_speed, train1_saved_speed;
 
 
 //statistic integerts
-int lost_money, error_count;
+int cycle, lost_money, error_count;
 
 
 //integers for (possible) losses
-int possible_loss;
+int possible_loss, delay_pessanger;
+
 
 // function if train is too late 
 void finished() {
@@ -36,59 +43,94 @@ void finished() {
 	train1_time = 0;
 }
 
+
+// Calculates best option when a train wont arrive in time.
 void best_option() {
 	//cout << "calculating best option" << endl;
-	cout << (train1_left + train1_current) - train1_aproximet << endl;
 
 	if ((train1_left + train1_current) - train1_aproximet >= 60 &&  (train1_left + train1_current) - train1_aproximet < 120){
 		cout << "One train is at least 60 minutes late!" << endl;
+		// 25% loss of income per ticket
 	}
 	if ((train1_left + train1_current) - train1_aproximet >= 120){
 		cout << "One train is at least 120 minutes late!" << endl;
+		// 50% loss of income per ticket
 	}
 
 
 }
+
+
+// Logs all important values to console.
 void log() {				
 	cout << "left: " << train1_left << endl;
 	cout << "current: " << train1_current <<endl;
-	cout << "aproximet left: " <<  (train1_aproximet - train1_current / (train1_speed / 60)) - 1<< endl; 
+	cout << "aproximet left: " <<  (train1_aproximet - train1_current / (train1_speed)) << endl; 
 	cout << "length: " << train1_length << endl;
-	cout << "aproximet" << train1_aproximet << endl;
+	cout << "aproximet: " << train1_aproximet << endl;
+	cout << "cycle: " << cycle << endl;
+
+}
+
+
+// Creates Problems
+void create_problem() {
+
+	if (1 + (float)(rand()) / ((float)(RAND_MAX/(2 - 1))) <= 1.5){
+		cout << "A problem accured.";		
+		train1_speed = 10;
+	}
+
+
 }
 
 
 //main function
 int main() {
-	train1_length = 50;
-	train1_speed = 400;
-	train1_aproximet = (train1_length / (train1_speed / 60)) + 1;	 
-	cout << "In this simultaion one second is equal to one minute!" << endl; 
-	
-	// Only for testing purposes, to trigger delays :
-	train1_speed = 200;		
-	for(;;)
-	{
 
-		log()
+	// Sets values for example train1
+	train1_length = 50;
+	train1_speed = 25;
+	train1_aproximet = (train1_length / train1_speed);	 
+	cout << "In this simultaion one second is equal to one minute!" << endl; 
+	delay_pessanger = 50;
+	train1_saved_speed = train1_speed;
 	
-		train1_left = (train1_length - train1_current) / (train1_speed / 60) ;	
+	for(;;)
+	{	// Resets existing problems
+		train1_speed = train1_saved_speed;
+
+
+		// Triggers creation of roblems
+		create_problem();	
 	
-		if (train1_current >= train1_length){
-			finished();
-		}else{
-			if (train1_left > (train1_aproximet - train1_current / (train1_speed / 60)) - 1){
-				// A train probaply wont arrive in time!
-				cout << "A train wont arrive in time." << endl;
-				best_option();
+		// Used for logging
+		cycle++;
+		
+		train1_time++;
+		train1_current = train1_current + train1_speed;	
+		train1_left = (train1_length - train1_current) / (train1_speed );
 	
-			}
-			train1_time++;
-			train1_current = train1_current + train1_speed / 60;
-				
+		// Checks if train will arrive in time
+		if (train1_left > (train1_aproximet - train1_current / (train1_speed))){
+			// A train probaply wont arrive in time!
+			cout << "A train wont arrive in time." << endl;
+			best_option();
 		}
 
-		Sleep(100);
+		// Checks if Train arrived its destination
+		if (train1_current >= train1_length){
+			finished();
+		}	
+	
+	log();
+	
+	cout << "###################################" << endl;
+	
+	Sleep(1000);
+	
 	}
 }
 
+
+// Sets values for example train1
